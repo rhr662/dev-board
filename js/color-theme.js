d8cacff045e7da742524bfc6e275d6702bad7e86
rhr662 <rhrabbi662@gmail.com>
2025-03-01 02:01:52 +0600

    first commit (navbar complate & color theme btn done

---
+++ js/color-theme.js
@@ -0,0 +1,11 @@
+function getRandomColor () {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+}
+
+
+
+
+document.getElementById("theme-btn").addEventListener("click", function () {
+    document.body.style.backgroundColor = getRandomColor();
+})
